3161f624554909a397b1ec200b3737674f8f7ec8
ShreyashMahajan <90761513+ShreyashMahajan@users.noreply.github.com>
2022-02-06 16:17:21 +0530

    feat: responsive img, toast and text utility added

--- component_documentation/componentDoc.js
+++ component_documentation/componentDoc.js
@@ -6,9 +6,27 @@ const menuClose = document.querySelector(".icon-cross");
 menuHamburger.addEventListener("click", () => {
     sideBar.style.width = "70%";
     sideBar.style.display = "initial";
-    mainBar.style.display = "none";
     menuHamburger.style.display = "none";
     menuClose.style.display = "initial";
+
+    function mediaQueryFunction(x) {
+      if (x.matches) { // If media query matches
+        sideBar.style.width = "70%";
+        sideBar.style.display = "initial";
+        mainBar.style.display = "";
+        menuHamburger.style.display = "none";
+        menuClose.style.display = "initial";
+      } else {
+        sideBar.style.display = "initial";
+        sideBar.style.width = "20rem";
+        menuHamburger.style.display = "none";
+        menuClose.style.display = "none";
+      }
+    }
+
+    let mediaWidth = window.matchMedia("(max-width: 700px)")
+    mediaQueryFunction(mediaWidth) // Call listener function at run time
+    mediaWidth.addListener(mediaQueryFunction);
 
 })
 
